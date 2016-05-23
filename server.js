18fb4b3dafc8c18e187a9d645493c6f0ac984a81
rayGreycloud <rayGreycloud@gmail.com>
2016-05-23 14:09:43 -0700

    refactor API with underscore

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 // initialize variables
 var PORT = process.env.PORT || 3000;
@@ -27,14 +28,7 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo;
-
-  todos.forEach(function(todo) {
-    if (todoId === todo.id) {
-      console.log("found it!");
-      matchedTodo = todo;
-    }
-  });
+  var matchedTodo = _.findWhere(todos, {id: todoId});
 
   if (matchedTodo) {
     res.json(matchedTodo);
@@ -45,7 +39,17 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos
 app.post('/todos', function(req, res) {
-  var body = req.body;
+  // eliminate extra fields
+  var body = _.pick(req.body, ['description', 'completed']);
+
+  // valid input fields
+  if (!_.isBoolean(body.completed) || !_.isString(body.description)
+  || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+
+  // trim body description
+  body.description = body.description.trim();
 
   // add id field and increment id number
   body.id = todoNextId;
