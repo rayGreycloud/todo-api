475953484e1e7f45335e03623edf878b4ea1e119
rayGreycloud <rayGreycloud@gmail.com>
2016-05-27 06:37:02 -0700

    add authenticate class method

--- server.js
+++ server.js
@@ -143,30 +143,16 @@ app.post('/users/login', function(req, res) {
   // eliminate extra fields
   var body = _.pick(req.body, 'email', 'password');
 
-  if (typeof body.email !== 'string' || typeof body.password !== 'string') {
-    return res.status(400).send();
-  }
-
-  db.user.findOne({
-    where: {
-      email: body.email
-    }
-  }).then(function(user) {
-    if (!user || !bcrypt.compareSync(body.password, user.get(
-        'password_hash'))) {
-      return res.status(401).send();
-    }
-
+  db.user.authenticate(body).then(function(user) {
     res.json(user.toPublicJSON());
-
   }, function(e) {
-    res.status(500).send();
+    return res.status(401).send();
   });
 });
 
-
-
-db.sequelize.sync().then(function() {
+db.sequelize.sync({
+  force: true
+}).then(function() {
   app.listen(PORT, function() {
     console.log('Express listening on port ' + PORT + '...');
   });
